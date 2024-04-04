12336a4ee3b52202043c8005ce0b2119a0eda6e5
Anton Orlichenko <aorliche@gmail.com>
2024-04-04 15:38:40 -0500

    Hunimal footer

--- counter.php
+++ counter.php
@@ -15,11 +15,28 @@
     $sql = "SELECT count FROM visitor_count WHERE idx = 1";
     $result = $conn->query($sql);
 
+    function likesToHunimal($likes) {
+        if ($likes == 0) {
+            return "&#x5500";
+        }
+        $hun = "";
+        while ($likes > 0) {
+            $digit = $likes % 100;
+            if ($digit < 10) {
+                $digit = "0$digit";
+            }
+            $hun = "&#x55$digit;$hun";
+            $likes = intval($likes/100);
+        }
+        return $hun;
+    }
+
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             $visits = $row["count"];
         }
-        printf("%05d", $visits);
+        //printf("%05d", $visits);
+        echo likesToHunimal($visits);
     } else {
         echo "no results";
     }
