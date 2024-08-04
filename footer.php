ba832a40c50d7986277a27fcb365435ff0d68169
Anton Orlichenko <aorliche@gmail.com>
2024-08-04 17:18:54 -0500

    Updated footer year

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-    <p>&#169; 2020-22 Site Owners</p>
+    <p>&#169; 2020-24 Site Owners</p>
     <p class='hunimal-font'><? include('counter.php'); ?> visitors since December 2020.</p>
 	<p><a href='/Contact.php'>Contact the owners</a></p>
 </div>
