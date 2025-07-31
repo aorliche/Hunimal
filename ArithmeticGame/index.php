5939e4d149fc8f701ac048ee60178dcd5e0e94ac
Anton Orlichenko <aorliche@gmail.com>
2025-07-31 11:40:55 -0500

    Iframe arithmetic game

---
+++ ArithmeticGame/index.php
@@ -0,0 +1,23 @@
+<? set_include_path('/home3/calmprepared/public_html/'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Arithmetic Game</title>
+    <!-- Generic Hunimal -->
+    <? include('generic.php'); ?>
+    <!-- Hunimal-specific -->
+	<link rel="stylesheet" href="style/index.css">
+</head>
+<body>
+    <h1>Hunimal Arithmetic Game</h1>
+    <? include('menu.php'); ?>
+    <div id="container">
+        <? include('navbar.php'); ?>
+        <div id="main">
+			<p>How well can you do arithmetic in Hunimal? Try to beat the timer and get the best score!</p>
+			<iframe src='/arithmetic-game/index.html' width=900 height=700></iframe>
+        </div>
+    </div>
+    <? include('footer.php'); ?>
+</body>
+</html>
