84a3af9511bf807618604e2c5434770513c9f70b
Anton Orlichenko <aorliche@gmail.com>
2025-08-10 18:08:33 -0500

    Arithmetic game numpad

--- ArithmeticGame/index.php
+++ ArithmeticGame/index.php
@@ -15,7 +15,7 @@
         <? include('navbar.php'); ?>
         <div id="main">
 			<p>How well can you do arithmetic in Hunimal? Try to beat the timer and get the best score!</p>
-			<iframe src='/arithmetic-game/index.html' width=900 height=700></iframe>
+			<iframe src='/arithmetic-game/index.html' width=1050 height=700></iframe>
         </div>
     </div>
     <? include('footer.php'); ?>
