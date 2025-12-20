fb1e54c1e54816ccaaf354c3a56fd67600a78cbf
Anton Orlichenko <aorliche@gmail.com>
2025-12-20 16:21:34 -0500

    Saved centimal

--- Hunimal/index.php
+++ Hunimal/index.php
@@ -18,18 +18,21 @@
 			<h2>Hunimal Numbers!</h2>
             <p>
             <a id='showenglish' href='#english'>English</a>
-            <a id='showver2' href='#ver2'>English (V2)</a>
+			<a id='showver2' href='#ver2'>V2</a>
+			<a id='showcentimal' href='#centimal'>Centimal</a>
             <a id='showspanish' href='#spanish'>Spanish</a>
             <a id='showrussian' href='#russian'>Russian</a>
             <a id='showgerman' href='#german'>German</a>
             <a id='showkorean' href='#korean'>Korean</a>
-            <a id='showswedish' href='#swedish'>Swedish</a>
+			<a id='showswedish' href='#swedish'>Swedish</a>
             </p>
 			<p>Check out the table below for the hunimal numbers 00-99.</p>
             <h3 id='english' style='display: none;'>English (Hunimal) Version 1</h3>
 			<table id="hunimal-list-table" class='huntab' style='display: none;'></table>
             <h3 id='ver2'>English (Hunimal) Version 2</h3>
 			<table id="hunimal-ver2" class='huntab'></table>
+            <h3 id='centimal' style='display: none;'>English (Competing Centimal System)</h3>
+			<table id="hunimal-centimal" class='huntab' style='display: none;'></table>
             <h3 id='spanish' style='display: none;'>Spanish (Cienimal)</h3>
             <table id="hunimal-spanish" class='huntab' style='display: none;'></table>
             <h3 id='russian' style='display: none;'>Russian (Sotimal)</h3>
@@ -39,7 +42,8 @@
             <h3 id='korean' style='display: none;'>Korean (Baekimal)</h3>
             <table id="hunimal-korean" class='huntab' style='display: none;'></table>
             <h3 id='swedish' style='display: none;'>Swedish (Hunärt)</h3>
-            <table id='hunimal-swedish' class='huntab' style='display: none;'></table>
+			<table id='hunimal-swedish' class='huntab' style='display: none;'></table>
+			<p id='centimal-exp' style='display: none;'>The Centimal system, originally found at <a href='https://centimal.org'>https://centimal.org</a>, is a competing though inferior system to Hunimal. As of December 2025 the site can be reached via the Internet Archive Wayback Machine. Their most significant feature was the Centimal geolocation service.</p>
         </div>
     </div>
     <? include('footer.php'); ?>
--- Hunimal/script/index.js
+++ Hunimal/script/index.js
@@ -3,26 +3,29 @@ window.addEventListener("load", e => {
 	let hunNum2Words = null;
 	const hunimalListTable = document.querySelector("#hunimal-list-table");
     const ver2 = document.querySelector('#hunimal-ver2');
+	const centimal = document.querySelector('#hunimal-centimal');
     const spanish = document.querySelector("#hunimal-spanish");
     const russian = document.querySelector("#hunimal-russian");
     const german = document.querySelector("#hunimal-german");
     const korean = document.querySelector("#hunimal-korean");
     const swedish = document.querySelector('#hunimal-swedish');
 
-    const alltables = [hunimalListTable, ver2, spanish, russian, german, korean, swedish];
+    const alltables = [hunimalListTable, ver2, centimal, spanish, russian, german, korean, swedish];
 
     const henglish = document.querySelector('#english');
     const hver2 = document.querySelector('#ver2');
+	const hcentimal = document.querySelector('#centimal');
     const hspanish = document.querySelector('#spanish');
     const hrussian = document.querySelector('#russian');
     const hgerman = document.querySelector('#german');
     const hkorean = document.querySelector('#korean');
     const hswedish = document.querySelector('#swedish');
 
-    const allh = [henglish, hver2, hspanish, hrussian, hgerman, hkorean, hswedish];
+    const allh = [henglish, hver2, hcentimal, hspanish, hrussian, hgerman, hkorean, hswedish];
     
     const aenglish = document.querySelector('#showenglish');
     const aver2 = document.querySelector('#showver2');
+	const acentimal = document.querySelector('#showcentimal');
     const aspanish = document.querySelector('#showspanish');
     const arussian = document.querySelector('#showrussian');
     const agerman = document.querySelector('#showgerman');
@@ -36,6 +39,8 @@ window.addEventListener("load", e => {
         allh.forEach(h => {
             h.style.display = "none";
         });
+		const centExp = document.querySelector('#centimal-exp');
+		centExp.style.display = "none";
     }
 
     function show(tab, h) {
@@ -53,6 +58,13 @@ window.addEventListener("load", e => {
         hideall();
         show(ver2, hver2);
     });
+	acentimal.addEventListener('click', e => {
+		e.preventDefault();
+		hideall();
+		show(centimal, hcentimal);
+		const centExp = document.querySelector('#centimal-exp');
+		centExp.style.display = "block";
+	});
     aspanish.addEventListener("click", e => {
         e.preventDefault();
         hideall();
@@ -121,6 +133,7 @@ window.addEventListener("load", e => {
     
 	fetchWordsAndStart(hunimalListTable, "words/hun.txt", '\r\n');
 	fetchWordsAndStart(ver2, "words/hun2.txt", '\n');
+	fetchWordsAndStart(centimal, "words/Centimal.txt", '\n');
 	fetchWordsAndStart(spanish, "words/cienimal.txt", '\r\n');
 	fetchWordsAndStart(russian, "words/sotimal_cyrillic.txt", '\n');
 	fetchWordsAndStart(german, "words/Dertimal.txt", '\n');
