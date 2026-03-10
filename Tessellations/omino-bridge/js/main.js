2f3d57a5cfd01c5fb3d89bb4c5c66eb9f08e37e9
Anton Orlichenko <aorliche@gmail.com>
2026-03-10 10:52:09 -0500

    Added omino bridge first draft

---
+++ Tessellations/omino-bridge/js/main.js
@@ -0,0 +1,188 @@
+
+import {$, $$} from './util.js';
+import {Point, dist} from './primitives.js';
+import {Edge, Polygon, getNewPolyPointsFromEdge, getNewPolyPointsFromExisting, createPolyFromPointsAndIntegrate, automaticallyGrow} from './polys.js';
+
+window.addEventListener('load', e => {
+	const canvas = $('#canvas');	
+	const ctx = canvas.getContext('2d');
+	const h = canvas.height;
+	const w = canvas.width;
+
+	let edges = [];
+	let polys = [];
+	let selEdge = null;
+
+	canvas.addEventListener('click', e => {
+		// Unselect
+		for (let i=0; i<edges.length; i++) {
+			edges[i].selected = false;
+		}
+		selEdge = null;
+		const x = e.offsetX;
+		const y = e.offsetY;
+		const p = Point(x,y);
+		for (let i=0; i<edges.length; i++) {
+			if (edges[i].click(p)) {
+				edges[i].selected = true;
+				selEdge = edges[i];
+				break;
+			}
+		}
+		// We may have clicked inside a polygon: delete it
+		if (selEdge == null) {
+			for (let i=0; i<polys.length; i++) {
+				const poly = polys[i];
+				// Delete poly and fix up edges
+				if (poly.contains(p)) {
+					polys.splice(i,1);
+					for (let j=0; j<edges.length; j++) {
+						for (let k=0; k<edges[j].polys.length; k++) {
+							if (edges[j].polys[k] == poly) {
+								edges[j].polys.splice(k,1);
+								k--;
+							}
+							if (edges[j].polys.length == 0) {
+								edges.splice(j,1);
+								j--;
+							}
+						}
+					}
+					break;
+				}
+			}
+		}
+	});
+
+	canvas.addEventListener('keydown', e => {
+		let n = parseInt(e.key);
+		if (!([3, 4, 6, 1].includes(n))) {
+			console.log("Unrecognized key code " + e.keyCode);
+			return;
+		}
+		if (n == 1) {
+			n = 12;
+		}
+		if (selEdge && selEdge.polys.length < 2) {
+			// This is the first edge
+			if (selEdge.polys.length == 0) {
+				const points = getNewPolyPointsFromEdge(selEdge, n, false);
+				createPolyFromPointsAndIntegrate(points, selEdge, polys, edges);
+			// This is constructed from the side of an existing poly
+			} else {
+				const points = getNewPolyPointsFromExisting(selEdge.polys[0], selEdge, n);
+				createPolyFromPointsAndIntegrate(points, selEdge, polys, edges);
+			}
+		}
+	});
+
+	$('#new').addEventListener('click', e => {
+		e.preventDefault();
+		initialized = false;
+		nFrame = 0;
+		nGrow = 0;
+		boards = [];
+	});
+
+	[3, 4, 6, 1].forEach(key => {
+		$('#k' + key).addEventListener('click', e => {
+			e.preventDefault();
+			const keydown = new KeyboardEvent('keydown', {key});
+			canvas.dispatchEvent(keydown);
+		});
+	});
+
+	// Try to create n tessellations, then choose the largest one
+	// Do this in an animation on the board
+	const N_GROWINGS = 5;
+	const N_ITER_PER_FRAME = 100;
+	const N_FRAMES = 15;
+	const N_START_POLYS = 30;
+	let boards = [];
+	let nFrame = 0;
+	let nGrow = 0;
+	let initialized = false;
+
+	let lastts = null;
+	function animate(ts) {
+		if (lastts === null) {
+			lastts = ts;
+			repaint();
+		} else if (ts - lastts > 1000/30) {
+			if (boards.length < N_GROWINGS) {
+				if (nFrame == 0) {
+					polys = [];
+					edges = [];
+					// Primordial edge
+					edges.push(new Edge(Point(w/2-20, h/2), Point(w/2+20, h/2)));
+				}
+				if (nFrame < N_FRAMES) {
+					let failed = false;
+					for (let i=0; i<N_ITER_PER_FRAME; i++) {
+						try {
+							automaticallyGrow(polys, edges);
+						} catch (e) {
+							failed = true;
+							break;
+						}
+					}
+					if (failed) {
+						nFrame = 0;
+					} else {
+						nFrame++;
+					}
+				}
+				if (nFrame == N_FRAMES) {
+					boards.push({polys, edges});
+					nFrame = 0;
+				}
+			} else if (!initialized) {
+				boards.sort((a,b) => a.polys.length < b.polys.length);
+				// Get a random subset of polys
+				const initPolys = boards[0].polys;
+				polys = [];
+				edges = [];
+				let i = 0;
+				while (polys.length < N_START_POLYS) {
+					const poly = initPolys[Math.floor(Math.random()*initPolys.length)];
+					if (polys.includes(poly)) {
+						continue;
+					}
+					const center = poly.center;
+					if (center.x < 100 
+						|| center.x > canvas.width-100
+						|| center.y < 100
+						|| center.y > canvas.height-100) {
+						continue;
+					}
+					const succ = createPolyFromPointsAndIntegrate(poly.points, null, polys, edges);
+					if (!succ) {
+						continue;
+					}
+					// Safety
+					if (i++ >= 100) {
+						break;
+					}
+				}
+				initialized = true;
+			}
+			lastts = ts;
+			repaint();
+		}
+		requestAnimationFrame(animate);
+	}
+
+	function repaint() {
+		ctx.clearRect(0, 0, w, h);
+		for (let i=0; i<polys.length; i++) {
+			polys[i].draw(ctx, false);
+		}
+		if (selEdge) {
+			ctx.strokeStyle = 'red';
+			selEdge.draw(ctx);
+			ctx.strokeStyle = 'black';
+		}
+	}
+
+	animate(0);
+});
---
+++ Tessellations/omino-bridge/js/polys.js
@@ -0,0 +1,447 @@
+
+import {shuffle} from './util.js';
+import {Point, clonePoint, add, sub, mul, rotate, nearby, nearbyScalar, dist} from './primitives.js';
+export {Edge, Polygon, getNewPolyPointsFromExisting, getNewPolyPointsFromEdge, createPolyFromPointsAndIntegrate, cullEdgesAndPolysNearVertex, automaticallyGrow};
+
+function automaticallyGrow(globPolys, globEdges) {
+	if (globEdges.length == 1 && globPolys.length == 0) {
+		const ns = [3, 4, 6, 12];
+		shuffle(ns);
+		const n = ns[0];
+		const points = getNewPolyPointsFromEdge(globEdges[0], n, false);
+		createPolyFromPointsAndIntegrate(points, globEdges[0], globPolys, globEdges);
+		return;
+	}
+	// Choose closest edge with < 2 polygons on it
+	let closestDist = Infinity;
+	let closestEdge = null;
+	const center = globEdges[0].points[0];
+	for (let i=0; i<globEdges.length; i++) {
+		const e = globEdges[i];
+		if (e.polys.length == 2) {
+			continue;
+		}
+		const d0 = dist(e.points[0], center);
+		const d1 = dist(e.points[1], center);
+		if (d0 < closestDist) {
+			closestDist = d0;
+			closestEdge = e;
+		}
+		if (d1 < closestDist) {
+			closestDist = d1;
+			closestEdge = e;
+		}
+	}
+	if (closestEdge == null) {
+		throw new Error('Null closest edge');
+	}
+	// We give preference to trying dodecagons since they tend to disappear completely
+	// If all polygons were given equal try weights
+	// If this value is not just right, we instead get a preponderance of dodecagons
+	// If this value is too low (try everything randomly) we get too much backtracking?
+	let ns = null;
+	if (Math.random() > 0.5) {
+		ns = [3, 4, 6, 12];
+		shuffle(ns);
+	} else {
+		ns = [3, 4, 6];
+		shuffle(ns);
+		ns.splice(0,0,12);
+	}
+	// If this doesn't work out we try again in outer loop
+	let succ = false;
+	for (let i=0; i<ns.length; i++) {
+		const n = ns[i];
+		const points = getNewPolyPointsFromExisting(closestEdge.polys[0], closestEdge, n);
+		succ = createPolyFromPointsAndIntegrate(points, closestEdge, globPolys, globEdges);
+		if (succ) {
+			break;
+		}
+	}
+	// Nothing works here so we backtrack
+	if (!succ) {
+		cullEdgesAndPolysNearVertex(closestEdge.points[0], globPolys, globEdges);
+		cullEdgesAndPolysNearVertex(closestEdge.points[1], globPolys, globEdges);
+	}
+}
+
+function getAngleRemainingAtVertex(newN, p, globEdges) {
+	let angle = 0;
+	switch (newN) {
+		case 3: angle += 60; break;
+		case 4: angle += 90; break;
+		case 6: angle += 120; break;
+		case 12: angle += 150; break;
+		default: throw new Error('Bad value for newN in getAngleRemaining'); break;
+	}
+	const uniqPolys = [];
+	for (let i=0; i<globEdges.length; i++) {
+		const e = globEdges[i];
+		if (nearby(e.points[0], p) || nearby(e.points[1], p)) {
+			for (let j=0; j<e.polys.length; j++) {
+				const poly = e.polys[j];
+				let found = false;
+				for (let k=0; k<uniqPolys.length; k++) {
+					if (uniqPolys[k] == poly) {
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					uniqPolys.push(poly);
+				}
+			}
+		}
+	}
+	for (let i=0; i<uniqPolys.length; i++) {
+		switch (uniqPolys[i].points.length) {
+			case 3: angle += 60; break;
+			case 4: angle += 90; break;
+			case 6: angle += 120; break;
+			case 12: angle += 150; break;
+			default: throw new Error('Bad value for points length in getAngleRemaining'); break;
+		}
+	}
+	return 360 - angle;
+}
+
+function getNewPolyPointsFromExisting(existing, edge, n) {
+	const pCW = getNewPolyPointsFromEdge(edge, n, false);
+	const pCCW = getNewPolyPointsFromEdge(edge, n, true);
+	const c = existing.center;
+	const cCW = centerFromPoints(pCW);
+	const cCCW = centerFromPoints(pCCW);
+	if (dist(c, cCW) < dist(c, cCCW)) {
+		return pCCW;
+	} else {
+		return pCW;
+	}
+}
+
+function getNewPolyPointsFromEdge(edge, n, ccw) {
+	const points = [clonePoint(edge.points[0]), clonePoint(edge.points[1])];
+	let theta = Math.PI - (n*Math.PI - 2*Math.PI)/n;
+	if (ccw == false) {
+		theta = -theta;
+	}
+	for (let i=0; i<n-1; i++) {
+		const dp = sub(points.at(-1), points.at(-2));
+		const dpr = rotate(dp, theta);
+		const dpn = add(points.at(-1), dpr);
+		points.push(dpn);
+	}
+	if (!nearby(points.at(-1), points[0])) {
+		throw new Error("Polygon does not close in on itself");
+	}
+	points.splice(n, 1);
+	return points;
+}
+
+function centerFromPoints(points) {
+	const n = points.length;
+	let center = Point(0,0);
+	for (let i=0; i<n; i++) {
+		center = add(center, mul(points[i], 1/n));
+	}
+	return center;
+}
+
+function cullEdgesAndPolysNearVertex(p, globPolys, globEdges) {
+	const sideLen = dist(globEdges[0].points[0], globEdges[0].points[1]);
+	const remEdges = [];
+	const remPolys = [];
+	const thresh = 1.1*sideLen;
+	for (let i=0; i<globEdges.length; i++) {
+		const e = globEdges[i];
+		// Directly removed edges
+		if (dist(e.points[0], p) < thresh || dist(e.points[1], p) < thresh) {
+			remEdges.push(e);
+			for (let j=0; j<e.polys.length; j++) {
+				remPolys.push(e.polys[j]);
+			}
+		}
+	}
+	// Removed polygons have effects on their edges
+	for (let i=0; i<globEdges.length; i++) {
+		const e = globEdges[i];
+		const polys = [];
+		for (let j=0; j<e.polys.length; j++) {
+			if (!remPolys.includes(e.polys[j])) {
+				polys.push(e.polys[j]);
+			}
+		}
+		if (polys.length == 0) {
+			if (!remEdges.includes(e)) {
+				remEdges.push(e);
+			}
+		} else {
+			e.polys = polys;
+		}
+	}
+	// Actually remove edges and polygons
+	for (let i=0; i<globEdges.length; i++) {
+		if (remEdges.includes(globEdges[i])) {
+			globEdges.splice(i, 1);
+			i--;
+		}
+	}
+	for (let i=0; i<globPolys.length; i++) {
+		if (remPolys.includes(globPolys[i])) {
+			globPolys.splice(i, 1);
+			i--;
+		}
+	}
+}
+
+function createPolyFromPointsAndIntegrate(points, selEdge, globPolys, globEdges) {
+	// Create poly
+	const poly = new Polygon({points});
+	// Check that all poly points are not inside an existing poly
+	for (let i=0; i<points.length; i++) {
+		for (let j=0; j<globPolys.length; j++) {
+			if (globPolys[j].contains(points[i])) {
+				console.log(`Polygon ${globPolys[j].id} contains point`);
+				return false;
+			}
+		}
+	}
+	// Check for compatible angle remaining
+	// When we choose random polys to populate the starting board, we may set selEdge to null
+	// Because we already know the tessellation is correct
+	if (selEdge != null) {
+		const angleRem0 = getAngleRemainingAtVertex(points.length, selEdge.points[0], globEdges);
+		const angleRem1 = getAngleRemainingAtVertex(points.length, selEdge.points[1], globEdges);
+		const allowed = [0,60,90,120,150,180,210,240,270,300]; 
+		if (!allowed.includes(angleRem0)) {
+			console.log(`Incompatible angle remaining ${angleRem0}`);
+			return false;
+		}
+		if (!allowed.includes(angleRem1)) {
+			console.log(`Incompatible angle remaining ${angleRem1}`);
+			return false;
+		}
+	}
+	// Assign edges, creating new ones if necessary
+	for (let i=0; i<poly.points.length; i++) {
+		const p0 = poly.points[i];
+		const p1 = poly.points[(i+1)%poly.points.length];
+		const temp = new Edge(p0, p1);
+		let found = false;
+		// We sometimes reach edge already has two polygons here
+		// If we do, remove that edge and its polys
+		for (let j=0; j<globEdges.length; j++) {
+			if (temp.equals(globEdges[j])) {
+				if (globEdges[j].polys.length >= 2) {
+					//console.log(globEdges[j]);
+					console.log("Edge already has two polygons");
+					cullEdgesAndPolysNearVertex(globEdges[j].points[0], globPolys, globEdges);
+					return false;
+				}
+				globEdges[j].polys.push(poly);
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			temp.polys.push(poly);
+			temp.id = edgeIdCount;
+			edgeIdCount++;
+			globEdges.push(temp);
+		}
+	}
+	globPolys.push(poly);
+	return true;
+}
+
+function getRandomHex() {
+	const digits = ['9','a','b','c','d','e','f'];
+	return digits[Math.floor(Math.random()*digits.length)];
+}
+
+function getRandomFill() {
+	return '#' + getRandomHex() + getRandomHex() + getRandomHex();
+}
+
+function getPointsOnLine(pStart, pEnd, nPoints) {
+	const points = [];
+	for (let i=0; i<nPoints; i++) {
+		const t = i/(nPoints-1);
+		const p = add(mul(pStart, t), mul(pEnd, 1-t));
+		points.push(p);
+	}
+	return points;
+}
+
+/*function getStartAndEndPolys(globPolys, width, height, PAD) {
+	const line0 = getPointsOnLine(Point(PAD,PAD), Point(PAD, height-PAD), 10);
+	const line1 = getPointsOnLine(Point(PAD, height-PAD), Point(width-PAD, height-PAD), 10);
+	const line2 = getPointsOnLine(Point(width-PAD, height-PAD), Point(width-PAD, PAD), 10);
+	const line3 = getPointsOnLine(Point(width-PAD, PAD), Point(PAD, PAD), 10);
+	const lines = line0.concat(line1).concat(line2).concat(line3);
+	const startP = lines[Math.floor(Math.random()*lines.length)];
+	// Get 80% of the distance from startP to the most distant point
+	const dists = [];
+	for (let i=0; i<lines.length; i++) {
+		const d = dist(startP, lines[i]);
+		dists.push({d, p: lines[i]});
+	}
+	dists.sort((a, b) => a.d < b.d);
+	const longDists = dists.slice(0, Math.floor(0.2*dists.length));
+	const endP = longDists[Math.floor(Math.random()*longDists.length)].p;
+	// Convert to polygons
+	let startPoly = null;
+	let endPoly = null;
+	for (let i=0; i<globPolys.length; i++) {
+		if (globPolys[i].contains(startP)) {
+			startPoly = globPolys[i];
+		}
+		if (globPolys[i].contains(endP)) {
+			endPoly = globPolys[i];
+		}
+		if (startPoly != null && endPoly != null) {
+			break;
+		}
+	}
+	return [startPoly, endPoly];
+}*/
+
+// Assuming we have two or more non-adjacent polys and we want to fill in edges from them
+/*function getEdgesFromPolys(globPolys) {
+	const edges = [];
+	for (let i=0; i<globPolys.length; i++) {
+		const points = globPolys[i].points;
+		for (let j=0; j<points.length; j++) {
+			const p0 = points[j];
+			const p1 = points[(j+1)%points.length];
+			edges.push(new Edge(p0, p1));
+			edges.at(-1).polys.push(globPolys[i]);
+		}
+	}
+	return edges;
+}*/
+
+let edgeIdCount = 1;
+
+class Edge {
+	constructor(p0, p1) {
+		this.points = [p0, p1];
+		this.polys = [];
+		this.selected = false;
+	}
+
+	click(p) {
+		const p0 = this.points[0];
+		const p1 = this.points[1];
+		for (let t=0; t<1.1; t += 0.2) {
+			const x = t*p0.x + (1-t)*p1.x;
+			const y = t*p0.y + (1-t)*p1.y;
+			if (nearby(Point(x,y), p, 8)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	draw(ctx, highlight) {
+		const p0 = this.points[0];
+		const p1 = this.points[1];
+		if (highlight || this.selected) {
+			ctx.save();
+			ctx.lineWidth = 3;
+			ctx.strokeStyle = 'red';
+		}
+		ctx.beginPath();
+		ctx.moveTo(p0.x, p0.y);
+		ctx.lineTo(p1.x, p1.y);
+		ctx.stroke();
+		if (highlight || this.selected) {
+			ctx.restore();
+		}
+	}
+
+	equals(edge) {
+		if (nearby(this.points[0], edge.points[0]) 
+			&& nearby(this.points[1], edge.points[1])) {
+			return true;
+		}
+		if (nearby(this.points[1], edge.points[0]) 
+			&& nearby(this.points[0], edge.points[1])) {
+			return true;
+		}
+		return false;
+	}
+}
+
+let polyIdCount = 1;
+
+class Polygon {
+	constructor(params) {
+		this.points = params.points ?? [];
+		if (params.id) {
+			this.id = params.id;
+		} else {
+			this.id = polyIdCount;
+			polyIdCount++;
+		}
+		this.fillStyle = getRandomFill();
+	}
+
+	get center() {
+		return centerFromPoints(this.points);
+	}
+
+	contains(point) {
+		// Corresponding vertices are okay
+		for (let i=0; i<this.points.length; i++) {
+			if (nearby(point, this.points[i])) {
+				return false;
+			}
+		}
+		// Since there is a lot of trouble when a ray passes through a vertex
+		// (e.g. hits twice when it should hit once, and the most logical
+		// change makes it hit zero times instead of once)
+		// We rotate the poly by a random angle about the point being tested
+		// Hope that randomness means we have a goes to zero chance of going 
+		// through a vertex and a goes to zero chance of having parallel lines
+		const theta = Math.random()*Math.PI/2;
+		const rotPoints = [];
+		for (let i=0; i<this.points.length; i++) {
+			const p = rotate(sub(this.points[i], point), theta);
+			rotPoints.push(p);
+		}
+		// Linear ray from point in direction of positive x
+		let cross = 0;
+		for (let i=0; i<rotPoints.length; i++) {
+			const p0 = rotPoints[i];
+			const p1 = rotPoints[(i+1)%rotPoints.length];
+			const t = (-p1.y) / (p0.y - p1.y);
+			if (t < 0 || t > 1) {
+				continue;
+			}
+			const x = t*p0.x + (1-t)*p1.x;
+			if (x > 0) {
+				cross++;
+			}
+		}
+		if ((cross%2) == 1) {
+			return true;
+		}
+		return false;
+	}
+
+	draw(ctx, showPolyId) {
+		ctx.beginPath();
+		ctx.moveTo(this.points[0].x, this.points[0].y);
+		for (let i=0; i<this.points.length; i++) {
+			const p = this.points[(i+1)%this.points.length];
+			ctx.lineTo(p.x, p.y);
+		}
+		ctx.fillStyle = this.fillStyle;
+		ctx.fill();
+		ctx.stroke();
+		if (showPolyId) {
+			const c = this.center;
+			ctx.fillText(this.id, c.x-5, c.y+5); 
+		}
+	}
+}
